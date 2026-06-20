d7ca3e20c5508dee055a65b71b8e087741bd0719
Salim Raza <rezasalim00011@gmail.com>
2026-06-20 12:20:23 +0600

    Add password change and OTP routes to URLs

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,4 +5,7 @@ from .views import *
 urlpatterns = [
     path("signup/", signup, name="signup"),
     path("signin/", signin, name="signin"),
+    path("change_password/", change_password, name="change_password"),
+    path("create_otp/", create_otp, name="create_otp"),
+    path("reset_password/", reset_password, name="reset_password")
 ]
