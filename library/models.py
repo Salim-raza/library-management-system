491b041d2040376f32200550b752a4c9b699c7cb
Salim Raza <rezasalim00011@gmail.com>
2026-06-22 23:16:11 +0600

    Modify Book model fields for publish and update_at
    Changed 'publish' field to DateTimeField and added 'update_at' field.

--- library/models.py
+++ library/models.py
@@ -5,9 +5,10 @@ from django.contrib.auth.models import User
 class Book(models.Model):
     title = models.CharField(max_length=100)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    publish = models.DateField(auto_now=True)
     Category = models.CharField(max_length=200)
     in_stock = models.CharField(max_length=20)
+    publish = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
     
     
     class Meta:
